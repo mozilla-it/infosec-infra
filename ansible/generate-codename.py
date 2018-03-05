dfe45b7adf60e161a988b128d26afd73888b0028
Scott Idler <scott.a.idler@gmail.com>
2018-03-05 12:55:18 -0800

    removed explicit path to python3 in /usr/local/ in favor of #!/usr/bin/env python3 in python script

--- ansible/generate-codename.py
+++ ansible/generate-codename.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 from faker import Faker
 fake = Faker()
 print("{}-{}{}".format(fake.safe_color_name(), fake.day_of_month(), fake.day_of_month()), end="")
